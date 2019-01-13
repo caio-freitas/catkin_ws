f7579b94f62659a899364de4f031e5c24058ebb4
Caio <caiofreitas@usp.br>
2019-01-13 17:32:16 -0200

    RTL

--- src/dronecontrol/flight_modes/RTL.py
+++ src/dronecontrol/flight_modes/RTL.py
@@ -5,65 +5,51 @@ import mavros_msgs
 from mavros_msgs import srv
 from geometry_msgs.msg import PoseStamped, TwistStamped
 from mavros_msgs.msg import State
-from sensor_msgs.msg import BatteryState
 import time
 
 ################# Objetos ############
 goal_pose = PoseStamped()
-current_state = State()
-Glocal = PoseStamped()
-local = PoseStamped()
-battery = BatteryState()
+drone_pose = PoseStamped()
+final_position = PoseStamped()
+
+final_position.pose.position.x = 0
+final_position.pose.position.y = 0
+final_position.pose.position.y = 0
+
+def chegou(goal, actual):
+    if (abs(goal.pose.position.x - actual.pose.position.x) < 0.1) and (abs(goal.pose.position.y - actual.pose.position.y) < 0.1) and (abs(goal.pose.position.z - actual.pose.position.z) < 0.10):
+        return True
+    else:
+        return False
+
+
+"""
+Function that brings the drone down to the (0,0,0) position and disarms it
+
+"""
 
 def drone_RTL():
     rospy.init_node('ReturnToLand', anonymous = True)
-    rate = rospy.Rate(20) # 10hz
-
+    rate = rospy.Rate(20)
+    velocity = 0.3
+    ds = velocity/20.0
     ############## Funcoes de Callback ########
-    def state_callback(state_data):
-        global current_state
-        current_state = state_data
-
-
     def local_callback(local):
-        global Glocal
+        global drone_pose
 
-        Glocal.pose.position.x = local.pose.position.x
-        Glocal.pose.position.y = local.pose.position.y
-        Glocal.pose.position.z = local.pose.position.z
-
-    def battery_callback(bat_dat):
-        global battery
-
-        battery.voltage = bat_dat.voltage
-        battery.percentage = bat_dat.percentage
-        battery.current = bat_dat.current
-
+        drone_pose.pose.position.x = local.pose.position.x
+        drone_pose.pose.position.y = local.pose.position.y
+        drone_pose.pose.position.z = local.pose.position.z
 
     ############### Publishers ###############
     local_position_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size = 100)
 
-
     ########### Subscribers ##################
     local_atual = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, local_callback)
-
-    state_status_subscribe = rospy.Subscriber('/mavros/state', State, state_callback)
-
-    battery_subscriber = rospy.Subscriber('/mavros/battery', BatteryState, battery_callback)
 
     ############## Services ##################
 
     arm = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
-
-    set_mode = rospy.ServiceProxy('/mavros/set_mode', mavros_msgs.srv.SetMode)
-
-    h=2
-
-    def chegou (goal, actual):
-        if (abs(actual.pose.position.z - goal.pose.position.z) < 0.05) and (abs(actual.pose.position.y - goal.pose.position.y) < 0.05) and (abs(actual.pose.position.x - goal.pose.position.x) < 0.05):
-            return True
-        return False
-
 
     def set_position(x, y, z):
         global goal_pose
@@ -72,39 +58,53 @@ def drone_RTL():
         goal_pose.pose.position.z = z
         local_position_pub.publish(goal_pose)
 
-
-
-    goal_pose = Glocal
-
-    for i in range(300):
-        local_position_pub.publish(goal_pose)
+    for i in range(100):
         rate.sleep()
 
     rospy.loginfo("[ROS] SETUP CONCLUIDO")
+    rate.sleep()
+    height = drone_pose.pose.position.z
+    rospy.loginfo('Position: (' + str(drone_pose.pose.position.x) + ', ' + str(drone_pose.pose.position.y) + ', ' + str(drone_pose.pose.position.z) + ')')
+
+    set_position(0,0,height)
+    rate.sleep()
+    rospy.loginfo('Position: (' + str(drone_pose.pose.position.x) + ', ' + str(drone_pose.pose.position.y) + ', ' + str(drone_pose.pose.position.z) + ')')
+    rospy.loginfo('Goal Position: (' + str(goal_pose.pose.position.x) + ', ' + str(goal_pose.pose.position.y) + ', ' + str(goal_pose.pose.position.z) + ')')
+
+
+    while not chegou(drone_pose, goal_pose):
+        rospy.loginfo('Executing State RTL')
+        rospy.loginfo ("STARING HOME")
+        set_position(0,0,height)
+        rate.sleep()
+
+    t=0
     set_position(0,0,0)
-    print("Position: ", Glocal.pose.position.x, Glocal.pose.position.y, Glocal.pose.position.z)
-    while not rospy.is_shutdown() and not chegou(goal_pose, Glocal):
-        if current_state != "OFFBOARD" or not current_state.armed:
-            arm(True)
-            set_mode(custom_mode = "OFFBOARD")
+    rate.sleep()
 
-        print(str(current_state.mode))
+    while not drone_pose.pose.position.z < 0.1:
+        rospy.loginfo('Executing State RTL')
 
-        if current_state.armed == True:
-            rospy.loginfo("DRONE ARMED")
+        rospy.loginfo('Height: ' + str(abs(drone_pose.pose.position.z)))
+        #print drone_pose
+        if not chegou(drone_pose, goal_pose):
+            rospy.logwarn ('LANDING AT ' + str(velocity) + 'm/s')
+            if t <= height:
+                t += ds
+            set_position(0,0,height - t)
+            rate.sleep()
 
-        if current_state.mode == "OFFBOARD":
-            rospy.loginfo('OFFBOARD mode setted')
+        else:
+            if t <= height:
+                t += ds
+                set_position(0,0,height - t)
+            else:
+                set_position(0,0,0)
+            rate.sleep()
 
-        rate.sleep()
-        #if not compara(Glocal, goal_pose):
-        print("Position: ", Glocal.pose.position.x, Glocal.pose.position.y, Glocal.pose.position.z)
-        set_position(0,0,0)
-        print ("COMING HOME")
-        rate.sleep()
     print("\nCHEGUEEEI\n")
+    rospy.logwarn("DESARMANDO DRONE")
     arm(False)
-    print("DESARMANDO DRONE")
     return "succeeded"
 
 if __name__ == "__main__":
